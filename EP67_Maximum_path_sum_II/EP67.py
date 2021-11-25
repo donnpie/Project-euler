c577ea59ab61ad78f6c495bd1bfd289327c2f0d8
Donn <donnpie@gmail.com>
2021-11-25 16:31:57 +0200

    Solved problem 67 (extension of problem 18)

--- EP67_Maximum_path_sum_II/EP67.py
+++ EP67_Maximum_path_sum_II/EP67.py
@@ -1,6 +1,6 @@
-# Project Euler problem #18
+# Project Euler problem #67
 # 2021/11/25
-# https://projecteuler.net/problem=18
+# https://projecteuler.net/problem=67
 
 # By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
 
@@ -11,32 +11,15 @@
 
 # That is, 3 + 7 + 4 + 9 = 23.
 
-# Find the maximum total from top to bottom of the triangle below:
+# Find the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target As...'), a 15K text file containing a triangle with one-hundred rows.
 
-# 75
-# 95 64
-# 17 47 82
-# 18 35 87 10
-# 20 04 82 47 65
-# 19 01 23 75 03 34
-# 88 02 77 73 07 63 67
-# 99 65 04 28 06 16 70 92
-# 41 41 26 56 83 40 80 70 33
-# 41 48 72 33 47 32 37 16 94 29
-# 53 71 44 65 25 43 91 52 97 51 14
-# 70 11 33 28 77 73 17 78 39 68 17 57
-# 91 71 52 38 17 14 91 43 58 50 27 29 48
-# 63 66 04 68 89 53 67 30 73 16 69 87 40 31
-# 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
-
-# NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route.
-# However, Problem 67, is the same challenge with a triangle containing one-hundred rows;
-# it cannot be solved by brute force, and requires a clever method! ;o)
-
-#import math
+# NOTE: This is a much more difficult version of Problem 18.
+# It is not possible to try every route to solve this problem, as there are 2^99 altogether!
+# If you could check one trillion (10^12) routes every second it would take over twenty billion years to check them all.
+# There is an efficient algorithm to solve it. ;o)
 
 # Read the numbers from a file - See resource 1
-with open('numbers.txt', 'r') as f:
+with open('p067_triangle.txt', 'r') as f:
     lines = f.readlines()
     f.close()
 
@@ -95,6 +78,17 @@ for c in range(1, cols):
 
 max_overall = max(total[rows-1]) # The max path sum can be found as the max value in the last row
 print(max_overall)
+
+
+
+
+
+
+
+
+
+
+
 
 # Resources
 # 01 EP13.py
